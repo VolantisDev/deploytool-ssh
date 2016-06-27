76d9e8ca4b4472d3d86da1b5dc2f638c6de0b255
Ben McClure <ben.mcclure@gmail.com>
2016-06-27 01:12:03 -0500

    Update to new plugin architecture of deploytool 0.2.0, version bump

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
  */
 
 module.exports = {
+  name: 'ssh',
+  tag: 'deployment',
+  init: function () {
+
+  },
   deploy: require('./lib/deploy'),
   sshCmdList: require('./lib/ssh-cmd-list'),
   sshKey: require('./lib/ssh-key')
